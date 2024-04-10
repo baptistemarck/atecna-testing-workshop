47771054b4a15a07a5139b4eaaef9b310024e299
Sam <samdelap@gmail.com>
2024-04-10 12:08:02 +0200

    chore: add vitest configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
+/// <reference types="vitest" />
+import path from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 4200,
+    host: true,
+  },
+  preview: {
+    port: 4300,
+    host: true,
+  },
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,38 @@
+import path from 'node:path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   test: {
+    root: './',
+    globals: true,
     environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.{ts,tsx}'],
+    exclude: ['node_modules/'],
+    setupFiles: ['./src/testing/setup.ts'],
+    clearMocks: true,
+    coverage: {
+      provider: 'v8',
+      reportOnFailure: true,
+      reporter: ['text', 'html', 'lcov'],
+      include: ['**/src/**/*.ts*'],
+      exclude: [
+        'node_modules/',
+        'public/',
+        'src/main.tsx',
+        '**/__mocks__/**/*',
+        '**/mocks/**/*',
+        '**/testing/*',
+        '**/types.*',
+        '**/*.test.*',
+        '**/*.spec.*',
+        '**/*.svg.*',
+        '**/*module.scss.d.ts',
+      ],
+    },
   },
 });
